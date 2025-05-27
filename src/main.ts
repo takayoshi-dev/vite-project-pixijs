2007fb6ccc4643eaf33e05e4f8b19ec9292be2ee
T.Tominaga <t.tominaga1130@gmail.com>
2025-05-27 22:17:55 +0900

    PixiJSを導入

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
-import { greet } from "./greet";
-import { isZero } from "./isZero";
+import { Application, Assets, Sprite } from "pixi.js";
 
-if (isZero(0)) {
-  console.log(greet("tominaga"));
-}
+(async () => {
+  const app = new Application();
+  await app.init({
+    width: 640,
+    height: 480,
+    background: "#1099bb",
+  });
+  document.body.appendChild(app.canvas);
+  const texture = await Assets.load("https://pixijs.com/assets/bunny.png");
+  const bunny = new Sprite(texture);
+  app.stage.addChild(bunny);
+  bunny.anchor.set(0.5);
+  bunny.x = app.screen.width / 2;
+  bunny.y = app.screen.height / 2;
+
+  app.ticker.add((time) => {
+    bunny.rotation += 0.1 * time.deltaTime;
+  });
+})();
