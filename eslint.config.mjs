a576674b30f6976d8fc80fd86bf913612bd62c8b
T.Tominaga <t.tominaga1130@gmail.com>
2025-05-27 19:16:42 +0900

    ESLintとPrettierを導入

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: ["**/dist/**", "**/docs/**", "**/assets/**"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  tseslint.configs.recommended,
+]);
